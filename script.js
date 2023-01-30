ca32df1ddc38675dfe1f2e82e23da893884d9332
Raiyan <80240062+legendlife@users.noreply.github.com>
2023-01-30 11:22:26 +0600

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student", "Developer", "Web-Designer", "Bot-Developer"],
+        strings: ["Student", "Developer", "Web-Designer", "Bot-Developer", "Linux-User"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student", "Developer", "Web-Designer", "Bot-Developer"],
+        strings: ["Student", "Developer", "Web-Designer", "Bot-Developer", "Linux-User"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
